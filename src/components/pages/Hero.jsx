ba37495d84ef98b3bf0fc9698af291f2778e0e3b
Joseph Enubiak <enubiakjoseph@gmail.com>
2023-10-25 12:47:00 +0100

    done

--- src/components/pages/Hero.jsx
+++ src/components/pages/Hero.jsx
@@ -4,97 +4,82 @@ import StarOutlineIcon from "@mui/icons-material/StarOutline";
 import { useNavigate } from "react-router-dom";
 import { motion } from "framer-motion";
 import axios from "axios";
+import { ThreeCircles } from "react-loader-spinner";
 
-const data = [
-  {
-    id: 1,
-    title: "The Shawshank Redemption",
-    year: 1994,
-    votes: 678790,
-    rating: 9.2,
-    rank: 1,
-    metascore: 80,
-    category: "Thriller",
-    director: "Frank Darabont",
-    runtime: 142,
-    revenue: 28.34,
-    actors: "Tim Robbins, Morgan Freeman, Bob Gunton, William Sadler",
-    imageUrl:
-      "https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_SX300.jpg",
-  },
-  {
-    id: 2,
-    title: "The Godfather",
-    year: 1972,
-    votes: 511495,
-    rating: 9.2,
-    rank: 2,
-    metascore: 100,
-    category: "Crime",
-    director: "Francis Ford Coppola",
-    runtime: 175,
-    revenue: 134.97,
-    actors: "Marlon Brando, Al Pacino, James Caan, Diane Keaton",
-    imageUrl:
-      "https://m.media-amazon.com/images/M/MV5BZjg2ZTM3OTgtY2ExMS00OGM4LTg3NDEtNjQ0MjJiZDFmMGFkXkEyXkFqcGdeQXVyMDY3OTcyOQ@@._V1_SX300.jpg",
-  },
-  {
-    id: 3,
-    title: "The Godfather: Part II",
-    year: 1974,
-    votes: 319352,
-    rating: 9.0,
-    rank: 3,
-    metascore: 90,
-    category: "Crime",
-    director: "Francis Ford Coppola",
-    runtime: 202,
-    revenue: 57.3,
-    actors: "Al Pacino, Robert De Niro, Robert Duvall, Diane Keaton",
-    imageUrl:
-      "https://m.media-amazon.com/images/M/MV5BNThjZDgwZTYtMjdmYy00ZmUyLTk4NTUtMzdjZmExODQ3ZmY4XkEyXkFqcGdeQXVyMjkzMDgyNTg@._V1_SX300.jpg",
-  },
-  {
-    id: 4,
-    title: "The Dark Knight",
-    year: 2008,
-    votes: 1839571,
-    rating: 9.0,
-    rank: 4,
-    metascore: 84,
-    category: "Action",
-    director: "Christopher Nolan",
-    runtime: 152,
-    revenue: 534.86,
-    actors: "Christian Bale, Heath Ledger, Aaron Eckhart, Michael Caine",
-    imageUrl:
-      "https://m.media-amazon.com/images/M/MV5BNTQ1OWQzODktMTY3Zi00OTQxLWExOTYtZTNjZjY5ZTY4M2UyXkEyXkFqcGdeQXVyMTAzMzk0NjAy._V1_SX300.jpg",
-  },
-];
+// const data = [
+//   {
+//     id: 1,
+//     title: "The Shawshank Redemption",
+//     year: 1994,
+//     votes: 678790,
+//     rating: 9.2,
+//     rank: 1,
+//     metascore: 80,
+//     category: "Thriller",
+//     director: "Frank Darabont",
+//     runtime: 142,
+//     revenue: 28.34,
+//     actors: "Tim Robbins, Morgan Freeman, Bob Gunton, William Sadler",
+//     imageUrl:
+//       "https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_SX300.jpg",
+//   },
+//   {
+//     id: 2,
+//     title: "The Godfather",
+//     year: 1972,
+//     votes: 511495,
+//     rating: 9.2,
+//     rank: 2,
+//     metascore: 100,
+//     category: "Crime",
+//     director: "Francis Ford Coppola",
+//     runtime: 175,
+//     revenue: 134.97,
+//     actors: "Marlon Brando, Al Pacino, James Caan, Diane Keaton",
+//     imageUrl:
+//       "https://m.media-amazon.com/images/M/MV5BZjg2ZTM3OTgtY2ExMS00OGM4LTg3NDEtNjQ0MjJiZDFmMGFkXkEyXkFqcGdeQXVyMDY3OTcyOQ@@._V1_SX300.jpg",
+//   },
+//   {
+//     id: 3,
+//     title: "The Godfather: Part II",
+//     year: 1974,
+//     votes: 319352,
+//     rating: 9.0,
+//     rank: 3,
+//     metascore: 90,
+//     category: "Crime",
+//     director: "Francis Ford Coppola",
+//     runtime: 202,
+//     revenue: 57.3,
+//     actors: "Al Pacino, Robert De Niro, Robert Duvall, Diane Keaton",
+//     imageUrl:
+//       "https://m.media-amazon.com/images/M/MV5BNThjZDgwZTYtMjdmYy00ZmUyLTk4NTUtMzdjZmExODQ3ZmY4XkEyXkFqcGdeQXVyMjkzMDgyNTg@._V1_SX300.jpg",
+//   },
+//   {
+//     id: 4,
+//     title: "The Dark Knight",
+//     year: 2008,
+//     votes: 1839571,
+//     rating: 9.0,
+//     rank: 4,
+//     metascore: 84,
+//     category: "Action",
+//     director: "Christopher Nolan",
+//     runtime: 152,
+//     revenue: 534.86,
+//     actors: "Christian Bale, Heath Ledger, Aaron Eckhart, Michael Caine",
+//     imageUrl:
+//       "https://m.media-amazon.com/images/M/MV5BNTQ1OWQzODktMTY3Zi00OTQxLWExOTYtZTNjZjY5ZTY4M2UyXkEyXkFqcGdeQXVyMTAzMzk0NjAy._V1_SX300.jpg",
+//   },
+// ];
 
-// const options = {
-//   method: "GET",
-//   url: "https://movie-database-alternative.p.rapidapi.com/",
-//   params: {
-//     s: "Avengers Endgame",
-//     r: "json",
-//     page: "1",
-//   },
-//   headers: {
-//     "X-RapidAPI-Key": "8b0e2549a6mshf618a56d4fdb5eep1b8b4bjsn2a4456c7d5e5",
-//     "X-RapidAPI-Host": "movie-database-alternative.p.rapidapi.com",
-//   },
-// };
-const options = {
-  method: "GET",
-  url: "https://imdb8.p.rapidapi.com/auto-complete",
-  params: { q: "game of thr" },
-  headers: {
-    "X-RapidAPI-Key": "8b0e2549a6mshf618a56d4fdb5eep1b8b4bjsn2a4456c7d5e5",
-    "X-RapidAPI-Host": "imdb8.p.rapidapi.com",
-  },
-};
 const Hero = () => {
+  const [value, setValue] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState([]);
+  const [search, setSearch] = useState([]);
+  const [error, setError] = useState("");
+
   const imageVariants = {
     hidden: {
       opacity: 0,
@@ -128,19 +113,23 @@ const Hero = () => {
       state: item,
     });
   };
-
-  const fetchData = async () => {
+  const fetchData = async (type) => {
+    setLoading(true);
     try {
-      const response = await axios.request(options);
-      console.log(response.data);
+      const api_key = "070899656df8d3a6ee25fecdf23183b7";
+      let response = await axios.get(
+        `https://api.themoviedb.org/3/trending/${type}/day?api_key=${api_key}&media_type=movie`
+      );
+      console.log("response ", response.data.results);
+      setData(response.data.results);
+      setSearch(response.data.results);
+      setLoading(false);
     } catch (error) {
-      console.error(error);
+      setError(error);
+    } finally {
+      setLoading(false);
     }
   };
-
-  //   const [loading, setLoading] = useState(true);
-  const [value, setValue] = useState("");
-  const [search, setSearch] = useState([]);
 
   const handleSearch = () => {
     const temp = data.filter((e) =>
@@ -149,13 +138,17 @@ const Hero = () => {
     setSearch(temp);
   };
   useEffect(() => {
+    setSearch(data);
+  }, []);
+
+  useEffect(() => {
     if (value === "") {
       setSearch(data);
     }
   }, [value]);
 
   useEffect(() => {
-    fetchData();
+    fetchData("movie");
   }, []);
 
   return (
@@ -184,7 +177,27 @@ const Hero = () => {
             <p className="text-lg ml-10">items</p>
           </div>
           <div className="flex mt-20" id="list">
-            {search.length ? (
+            {loading ? (
+              <ThreeCircles
+                height="100"
+                width="100"
+                color="#4fa94d"
+                wrapperStyle={{}}
+                wrapperClass=""
+                visible={true}
+                ariaLabel="three-circles-rotating"
+                outerCircleColor=""
+                innerCircleColor=""
+                middleCircleColor=""
+                height="100"
+                width="100"
+                radius="9"
+                color="white"
+                ariaLabel="loading"
+                wrapperStyle
+                wrapperClass
+              />
+            ) : search.length ? (
               Array.from(search).map((item) => (
                 <motion.div
                   key={item.id}
@@ -197,12 +210,14 @@ const Hero = () => {
                   onClick={() => handleClick(item.id, item)}
                 >
                   <img
-                    src={item.imageUrl}
+                    src={`https://image.tmdb.org/t/p/w300/${item.poster_path}`}
                     className="w-full h-[300px] rounded-lg -mt-2"
                   />
 
                   <p className="text-darkGrayishBlue ml-3">{item.title}</p>
-                  <p className="text-darkGrayishBlue ml-3">{item.year}</p>
+                  <p className="text-darkGrayishBlue ml-3">
+                    {item.release_date}
+                  </p>
                   <p className="text-darkGrayishBlue ml-3">{item.category}</p>
 
                   <div className="ml-3 gap-2 flex items-center">
@@ -210,7 +225,7 @@ const Hero = () => {
                       fontSize="medium"
                       sx={{ fill: "goldenrod" }}
                     />
-                    <p>{item.rating}</p>
+                    <p>{item.vote_average}</p>
                   </div>
                 </motion.div>
               ))
--- src/components/pages/SingleMovie.jsx
+++ src/components/pages/SingleMovie.jsx
@@ -33,55 +33,47 @@ const SingleMovie = () => {
         initial="hidden"
       >
         <div className="flex flex-col">
-          <img src={data.imageUrl} className="w-full lg:h-[800px]" />
+          <img
+            src={`https://image.tmdb.org/t/p/w300/${data.backdrop_path}`}
+            className="w-full lg:h-[500px]"
+          />
           <div className="flex flex-col mt-6">
             <h1 className="text-4xl font-bold font-eudoxus text-left text-darkGrayishBlue lg:text-6xl">
               {data.title}
             </h1>
-            <p className="text-darkGrayishBlue mt-4">{data.description}</p>
+            <p className="text-darkGrayishBlue mt-4 text-justify">
+              <span className="text-xl font-bold text-darkGrayishBlue lg:text-xl">
+                Overview:{" "}
+              </span>
+              {(" ", data.overview)}
+            </p>
             <div className="flex mt-4 md:hidden">
               <div className="flex items-center gap-2">
                 <StarOutlineIcon fontSize="medium" sx={{ fill: "goldenrod" }} />
-                <p>{data.rating}</p>
+                <p>{data.vote_average}</p>
               </div>
               <div className="flex items-center gap-2 ml-4">
                 <CalendarTodayIcon fontSize="medium" sx={{ fill: "red" }} />
-                <p>{data.year}</p>
+                <p>{data.release_date}</p>
               </div>
               <div className="flex items-center gap-2 ml-4">
                 <TheatersIcon fontSize="medium" sx={{ fill: "blue" }} />
-                <p>{data.category}</p>
+                <p>{data.media_type}</p>
               </div>
             </div>
             <div className="hidden md:flex md:mt-4">
               <div className="flex items-center gap-2">
                 <StarOutlineIcon fontSize="large" sx={{ fill: "goldenrod" }} />
-                <p className="text-lg">{data.rating}</p>
+                <p className="text-lg">{data.vote_average}</p>
               </div>
               <div className="flex items-center gap-2 ml-4">
                 <CalendarTodayIcon fontSize="large" sx={{ fill: "red" }} />
-                <p className="text-lg">{data.year}</p>
+                <p className="text-lg">{data.release_date}</p>
               </div>
               <div className="flex items-center gap-2 ml-4">
                 <TheatersIcon fontSize="large" sx={{ fill: "blue" }} />
-                <p className="text-lg">{data.category}</p>
+                <p className="text-lg">{data.media_type}</p>
               </div>
-            </div>
-            <div className="items-center gap-2 mt-4">
-              <p className="text-yellow-300 lg:text-lg">
-                <span className="text-xl font-bold text-darkGrayishBlue lg:text-xl">
-                  Director:{" "}
-                </span>
-                {(" ", data.director)}
-              </p>
-            </div>
-            <div className="items-center gap-2 mt-4">
-              <p className="text-yellow-300 lg:text-lg">
-                <span className="text-xl font-bold text-darkGrayishBlue lg:text-xl">
-                  Actors:{" "}
-                </span>
-                {(" ", data.actors)}
-              </p>
             </div>
           </div>
         </div>
